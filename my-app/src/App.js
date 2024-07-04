32e6a2ca6bd881392798c827e06d13899495c5a9
Chansreynit6 <sreynitchanngoun@gmail.com>
2024-07-04 07:29:13 +0700

    Your commit message

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Button from './compenents/Button';
+
+function App() {
+  return (
+    <div>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+      <Button/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/compenents/Button.jsx
@@ -0,0 +1,8 @@
+import "./Button.css";
+
+function Button (){
+    return <Button className="blue-button">Submit</Button>;
+}
+export default Button;
+
+
